0f9542adb561f620504872fb894e81b1effa2268
Carlos Bernardo <147115322+udagla5@users.noreply.github.com>
2026-08-03 02:42:50 -0300

    Revert "Dialogos Funcionais agora"

--- Assets/Scripts/Dialogo.cs
+++ Assets/Scripts/Dialogo.cs
@@ -3,18 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-[System.Serializable]
-public struct LinhaDialogo
-{
-    [TextArea(2, 5)]
-    public string texto;
-    public bool falaDoJogador;
-}
-
 public class Dialogo : MonoBehaviour
 {
     public TextMeshProUGUI textComponent;
-    public LinhaDialogo[] falas;
+    public string[] falas;
     public float velocidadeTexto;
     public GameObject personagem;
     [SerializeField] public PlayerMoviment player;
@@ -26,9 +18,6 @@ public class Dialogo : MonoBehaviour
     [SerializeField] public bool npcBarraca;
     [SerializeField] private CameraZoom scriptDeZoom;
     
-    public Color corNPC = Color.white;
-    public Color corJogador = Color.cyan;
-
     private int index;
     private bool posJogo = false;
     private GameObject btnIniciarTemp;
@@ -37,20 +26,21 @@ public class Dialogo : MonoBehaviour
     void Start()
     {
         textComponent.text = string.Empty;
+        ComecarDialogo();
     }
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            if (textComponent.text == falas[index].texto)
+            if (textComponent.text == falas[index])
             {
                 ProximaFala();
             }
             else
             {
                 StopAllCoroutines();
-                textComponent.text = falas[index].texto;
+                textComponent.text = falas[index];
             }
         }
 
@@ -71,18 +61,15 @@ public class Dialogo : MonoBehaviour
         }
     }
 
+    void ComecarDialogo()
+    {
+        index = 0;
+        StartCoroutine(DigitaFala());
+    }
+
     IEnumerator DigitaFala()
     {
-        if (falas[index].falaDoJogador)
-        {
-            textComponent.color = corJogador;
-        }
-        else
-        {
-            textComponent.color = corNPC;
-        }
-
-        foreach (char c in falas[index].texto.ToCharArray())
+        foreach (char c in falas[index].ToCharArray())
         {
             textComponent.text += c;
             yield return new WaitForSeconds(velocidadeTexto);
@@ -99,87 +86,52 @@ public class Dialogo : MonoBehaviour
         }
         else
         {
-            EncerrarDialogo();
-        }
-    }
-
-    private void EncerrarDialogo()
-    {
-        if(canvas)
-        {
-            canvas.SetActive(true);
-        }
-        
-        gameObject.SetActive(false);
-        if (personagem != null)
-        {
-            personagem.SetActive(false);
-        }
-
-        if(player)
-        {
-            player.podeMover = true;
-        }
-
-        if (posJogo)
-        {
-            if (btnIniciarTemp != null) btnIniciarTemp.SetActive(true);
-            if (btnSairTemp != null) btnSairTemp.SetActive(true);
-            posJogo = false;
-
-            if (scriptDeZoom != null)
+            if(canvas)
             {
-                scriptDeZoom.IniciarZoomParaMinigame();
+                canvas.SetActive(true);
+            }
+            
+            gameObject.SetActive(false);
+            if (personagem != null)
+            {
+                personagem.SetActive(false);
+            }
+
+            if(player)
+            {
+                player.podeMover = true;
+            }
+
+            if (posJogo)
+            {
+                if (btnIniciarTemp != null) btnIniciarTemp.SetActive(true);
+                if (btnSairTemp != null) btnSairTemp.SetActive(true);
+                posJogo = false;
+
+                if (scriptDeZoom != null)
+                {
+                    scriptDeZoom.IniciarZoomParaMinigame();
+                }
+            }
+            else if (npcBarraca)
+            {
+               if (scriptDeZoom != null)
+                {
+                    scriptDeZoom.IniciarZoomParaMinigame();
+                }
             }
         }
-        else if (npcBarraca)
-        {
-           if (scriptDeZoom != null)
-            {
-                scriptDeZoom.IniciarZoomParaMinigame();
-            }
-        }
-    }
-
-    public void IniciarDialogo(LinhaDialogo[] novasFalas)
-    {
-        falas = novasFalas;
-        index = 0;
-        posJogo = false;
-        textComponent.text = string.Empty;
-        
-        // Garante que o objeto pai (Canvas) esteja ativo na cena
-        if (transform.parent != null)
-        {
-            transform.parent.gameObject.SetActive(true);
-        }
-
-        gameObject.SetActive(true);
-        
-        if (personagem != null)
-        {
-            personagem.SetActive(true);
-        }
-
-        StartCoroutine(DigitaFala());
     }
 
     public void IniciarDialogoPosJogo(string fala, GameObject btnIniciar, GameObject btnSair)
     {
-        falas = new LinhaDialogo[1];
-        falas[0] = new LinhaDialogo { texto = fala, falaDoJogador = false };
+        falas = new string[] { fala };
         index = 0;
         posJogo = true;
         btnIniciarTemp = btnIniciar;
         btnSairTemp = btnSair;
 
-        if (transform.parent != null)
-        {
-            transform.parent.gameObject.SetActive(true);
-        }
-
         gameObject.SetActive(true);
-        
         if (personagem != null)
         {
             personagem.SetActive(true);
